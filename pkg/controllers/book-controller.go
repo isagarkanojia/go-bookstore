de9c50b147dfc313dda05a99704c0070d13edd29
Sagar Kanojia <isagarkanojia@gmail.com>
2022-09-04 22:14:49 +0530

    Add controller

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,13 @@
+//  package controllers
+
+//  import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"github.com/gorilla/mux"
+
+// 	"net/http"
+// 	"strconv"
+// 	"github.com/isagarkanojia/pkg/models"
+//  )
+
+//  var NewBook models.Book
