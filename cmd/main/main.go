85320b0e9136394254f4469eb6bc0fdb3045da24
Sagar Kanojia <isagarkanojia@gmail.com>
2022-09-04 21:16:06 +0530

    Add db funtions, main.go

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/isagarkanojia/go-bookstore/pkg/routes"
+)
+
+func main(){
+	r:=mux.NewRouter()
+
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/",r)
+
+	log.Fatal(http.ListenAndServe(":8082",r))
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -18,3 +18,27 @@ func init(){
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func(b *Book) createBook() *Book{
+  db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books;
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB){
+	 var getBook Book
+	 db:= db.Where("ID=?",Id).Find(&getBook)
+	 return &getBook, db
+}
+
+func DeleteBook(Id int64) Book{
+	var book Book
+	db.Where("ID=?",Id).Delete(book)
+	return book;
+}
