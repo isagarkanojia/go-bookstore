d52a1ca1452ed5d5b0a701eb53be34701a477d15
Sagar Kanojia <isagarkanojia@gmail.com>
2022-10-05 06:59:11 +0530

    fix api naming

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,8 +6,8 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router){
-	router.HandleFunc("/books/", controllers.CreateBooks).Methods("POST")
-	router.HandleFunc("/books/",controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/books", controllers.CreateBooks).Methods("POST")
+	router.HandleFunc("/books",controllers.GetBooks).Methods("GET")
 	router.HandleFunc("/books/{bookId}",controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/books/{bookId}",controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
